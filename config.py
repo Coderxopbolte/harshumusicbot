d2284a31497d20fdcc2763cc4237c588495df11b
Team-RedWine <120706332+Team-RedWine@users.noreply.github.com>
2023-03-03 10:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/WCFnetwork")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/WorldChattingFriendsWCF")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Red_Wine_Op")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
