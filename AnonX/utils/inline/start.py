ddbd4d942eb770ece50c828004cbff7fe1ebe811
Royalkifeelings <116754209+royalboycoder@users.noreply.github.com>
2023-03-20 19:59:52 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌹 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌹",
+                text="✮ 𝙰ᴅᴅ 𝙼ᴇ 𝚃ᴏ 𝚈ᴏᴜʀ 𝙶ʀᴏᴜᴘ ✮",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗛𝗲𝗹𝗽 🌹",
+                text="✮ 𝙷ᴇʟᴩ ✮",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🌹 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🌹", callback_data="settings_helper"
+                text="✮ 𝚂ᴇᴛᴛɪɴɢ𝚂 ✮", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,26 +30,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌹 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌹",
+                text="✮ 𝙰ᴅᴅ 𝙼ᴇ 𝚃ᴏ 𝚈ᴏᴜʀ 𝙶ʀᴏᴜᴘ ✮",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗛𝗲𝗹𝗽 🌹", callback_data="settings_back_helper"
+                text="✮ 𝙷ᴇʟᴩ ✮", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🌹", url=config.SUPPORT_GROUP
+                text="✮ 𝚂ᴜᴩᴩᴏʀᴛ ✮", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🌹 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌹", url=f"https://t.me/WCFnetwork",
+                text="✮ 𝙾ᴡɴᴇʀ'𝚇𝙳  ✮", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗢𝘄𝗻𝗲𝗿 🌹", url=f"https://t.me/Kalakar_Sangram",
+                text="✮ 𝚂ᴏᴜʀᴄᴇ ✮", url=config.UPSTREAM_REPO
             )
         ],
      ]
