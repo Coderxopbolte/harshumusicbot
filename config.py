b896c3f6b569620332a5a6f492b3c7e42a2fafbb
Royalkifeelings <116754209+royalboycoder@users.noreply.github.com>
2023-03-22 00:11:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1711510822").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Coderxopbolte/Vc-Player")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Royalboyamit/redwain")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
