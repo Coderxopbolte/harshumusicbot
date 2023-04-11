56a233e5a4ad7856099e41c8bc161404a9010b16
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-04-11 11:08:07 +0530

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -10,22 +10,22 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from ZedzeX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from ZedzeX.core.call import Zedze
-from ZedzeX.utils import seconds_to_min, time_to_seconds
-from ZedzeX.utils.channelplay import get_channeplayCB
-from ZedzeX.utils.database import is_video_allowed
-from ZedzeX.utils.decorators.language import languageCB
-from ZedzeX.utils.decorators.play import PlayWrapper
-from ZedzeX.utils.formatters import formats
-from ZedzeX.utils.inline.play import (livestream_markup,
+from AnonX.core.call import Zedze
+from AnonX.utils import seconds_to_min, time_to_seconds
+from AnonX.utils.channelplay import get_channeplayCB
+from AnonX.utils.database import is_video_allowed
+from AnonX.utils.decorators.language import languageCB
+from AnonX.utils.decorators.play import PlayWrapper
+from AnonX.utils.formatters import formats
+from AnonX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from ZedzeX.utils.database import is_served_user
-from ZedzeX.utils.inline.playlist import botplaylist_markup
-from ZedzeX.utils.logger import play_logs
-from ZedzeX.utils.stream.stream import stream
+from AnonX.utils.database import is_served_user
+from AnonX.utils.inline.playlist import botplaylist_markup
+from AnonX.utils.logger import play_logs
+from AnonX.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
